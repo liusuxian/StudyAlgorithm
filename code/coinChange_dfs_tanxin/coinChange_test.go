894c319a2687960848bbfe03be2a11d1b785a686
liusuxian <382185882@qq.com>
2021-03-17 03:45:17 +0800

    update

--- code/coinChange_dfs_tanxin/coinChange_test.go
+++ code/coinChange_dfs_tanxin/coinChange_test.go
@@ -1,4 +1,4 @@
-package coinChange_backtrack
+package coinChange_dfs_tanxin
 
 import (
     "math"
@@ -6,13 +6,16 @@ import (
     "testing"
 )
 
-// 零钱兑换 回溯法 超时
 func coinChange(coins []int, amount int) int {
+    if amount == 0 {
+        return 0
+    }
+
     ret := math.MaxInt64
     sort.Slice(coins, func(i, j int) bool {
         return coins[i] > coins[j]
     })
-    dfs(coins, amount, 0, &ret)
+    dfs(coins, amount, 0, 0, &ret)
 
     if ret == math.MaxInt64 {
         return -1
@@ -21,20 +24,18 @@ func coinChange(coins []int, amount int) int {
     return ret
 }
 
-func dfs(coins []int, amount, count int, ret *int) {
+func dfs(coins []int, amount, count, index int, ret *int) {
     if amount == 0 {
         *ret = min(*ret, count)
         return
     }
 
-    for _, coin := range coins {
-        if amount < coin {
-            continue
-        }
+    if index == len(coins) {
+        return
+    }
 
-        amount -= coin
-        dfs(coins, amount, count+1, ret)
-        amount += coin
+    for k := amount / coins[index]; k >= 0 && k+count < *ret; k-- {
+        dfs(coins, amount-k*coins[index], k+count, index+1, ret)
     }
 }
 
@@ -45,13 +46,13 @@ func min(a, b int) int {
 
     return a
 }
+
 func TestFun(t *testing.T) {
     t.Log(coinChange([]int{1, 2, 5}, 11))
     t.Log(coinChange([]int{2}, 3))
     t.Log(coinChange([]int{1}, 0))
     t.Log(coinChange([]int{1}, 1))
     t.Log(coinChange([]int{1}, 2))
-    // 以下超时
-    //t.Log(coinChange([]int{1, 2, 5}, 100))
-    //t.Log(coinChange([]int{186, 419, 83, 408}, 6249))
+    t.Log(coinChange([]int{1, 2, 5}, 100))
+    t.Log(coinChange([]int{186, 419, 83, 408}, 6249))
 }
