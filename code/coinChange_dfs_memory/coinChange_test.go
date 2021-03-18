0527b5cc1dacc56d8ce075f0698a8596fa90d00b
Liusuxian <382185882@qq.com>
2021-03-18 14:37:20 +0800

    update

--- code/coinChange_dfs_memory/coinChange_test.go
+++ code/coinChange_dfs_memory/coinChange_test.go
@@ -1,8 +1,8 @@
 package coinChange_dfs_memory
 
 import (
-    "fmt"
     "math"
+    "sort"
     "testing"
 )
 
@@ -11,30 +11,42 @@ func coinChange(coins []int, amount int) int {
         return 0
     }
 
-    ret := math.MaxInt64
-    dfs(coins, amount, 0, &ret)
+    // memo[n] 表示钱币n可以被换取的最少的硬币数，不能换取就为-1
+    memo := make([]int, amount)
+    sort.Slice(coins, func(i, j int) bool {
+        return coins[i] > coins[j]
+    })
+    return dfs(coins, memo, amount)
+}
 
-    if ret == math.MaxInt64 {
+func dfs(coins []int, memo []int, amount int) int {
+    if amount < 0 {
         return -1
     }
 
-    return ret
-}
-
-func dfs(coins []int, amount, count int, ret *int) {
-    fmt.Println(amount, count, *ret)
-    if amount < 0 {
-        return
-    }
-
     if amount == 0 {
-        *ret = min(*ret, count)
-        return
+        return 0
     }
 
-    for _, coin := range coins {
-        dfs(coins, amount-coin, count+1, ret)
+    if memo[amount-1] != 0 {
+        return memo[amount-1]
     }
+
+    minVal := math.MaxInt64
+    for _, coin := range coins {
+        ret := dfs(coins, memo, amount-coin)
+        if ret >= 0 && ret < minVal {
+            minVal = ret + 1
+        }
+    }
+
+    if minVal == math.MaxInt64 {
+        memo[amount-1] = -1
+    } else {
+        memo[amount-1] = minVal
+    }
+
+    return memo[amount-1]
 }
 
 func min(a, b int) int {
@@ -47,10 +59,11 @@ func min(a, b int) int {
 
 func TestFun(t *testing.T) {
     t.Log(coinChange([]int{1, 2, 5}, 11))
-    //t.Log(coinChange([]int{2}, 3))
-    //t.Log(coinChange([]int{1}, 0))
-    //t.Log(coinChange([]int{1}, 1))
-    //t.Log(coinChange([]int{1}, 2))
-    //t.Log(coinChange([]int{1, 2, 5}, 100))
-    //t.Log(coinChange([]int{186, 419, 83, 408}, 6249))
+    t.Log(coinChange([]int{2}, 3))
+    t.Log(coinChange([]int{1}, 0))
+    t.Log(coinChange([]int{1}, 1))
+    t.Log(coinChange([]int{1}, 2))
+    // 以下超时
+    t.Log(coinChange([]int{1, 2, 5}, 100))
+    t.Log(coinChange([]int{186, 419, 83, 408}, 6249))
 }
